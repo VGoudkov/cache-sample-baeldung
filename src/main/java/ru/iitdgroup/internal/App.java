4609daf2ca50ed8ad9161b0c33570f970bf53fa4
Victor Goudkov <vgoudkov@iitdgroup.ru>
2019-12-11 18:25:36 +0300

    Добавление примера с хранением byte[] и правильным закрытием кэшей в программе

--- src/main/java/ru/iitdgroup/internal/App.java
+++ src/main/java/ru/iitdgroup/internal/App.java
@@ -5,6 +5,7 @@ import javax.cache.CacheManager;
 import javax.cache.Caching;
 import javax.cache.configuration.MutableConfiguration;
 import javax.cache.spi.CachingProvider;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Hello world!
@@ -16,16 +17,16 @@ public class App
     {
         CachingProvider cachingProvider = Caching.getCachingProvider();
         CacheManager cacheManager = cachingProvider.getCacheManager();
-        MutableConfiguration<String, String> config
+        MutableConfiguration<String, byte[]> config
                 = new MutableConfiguration<>();
-        try (Cache<String, String> cache = cacheManager
-                .createCache("simpleCache", config)) {
-            cache.put("key1", "value1");
-            cache.put("key2", "value2");
+        Cache<String, byte[]> cache = cacheManager
+                .createCache("simpleCache", config);
+        cache.put("key1", "value1".getBytes(StandardCharsets.UTF_8));
+        cache.put("key2", "value2".getBytes(StandardCharsets.UTF_8));
 
-            cache.forEach(entry -> System.out.println(entry.getKey()+" : "+entry.getValue()));
-        }
+        cache.forEach(entry -> System.out.println(entry.getKey()+" : "+new String(entry.getValue())));
 
-
+        cachingProvider.close();
+        System.out.println("Should stop here");
     }
 }
